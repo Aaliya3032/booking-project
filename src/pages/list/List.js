8bf1c3b99ef632d48b1d543c7cc681b965e51cd8
Aaliya3032 <63299680+Aaliya3032@users.noreply.github.com>
2023-02-02 16:17:35 +0530

    Update List.js

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -10,10 +10,10 @@ import SearchItem from '../../component/searchItem/SearchItem'
 const List = () => {
 
   const location = useLocation();
-  //const [destination,setDestination]= useState(location.state.destination)
+  const [destination,setDestination]= useState(location.state.destination)
   const [date,setDate]= useState(location.state.date)
   const [openDate,setOpenDate]= useState(false)
-  //const [options,setOptions]= useState(location.state.options)
+  const [options,setOptions]= useState(location.state.options)
 
   return (
     <div> 
